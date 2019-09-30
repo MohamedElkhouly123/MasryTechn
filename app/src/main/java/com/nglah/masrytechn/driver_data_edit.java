8b9bb159d58c8987a6aa7d13ce42eb6b288cb6d8
moh <MohamedElkhouly123>
2019-09-30 02:14:08 +0200

    new

---
+++ app/src/main/java/com/nglah/masrytechn/driver_data_edit.java
@@ -0,0 +1,14 @@
+package com.nglah.masrytechn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class driver_data_edit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_data_edit);
+    }
+}
