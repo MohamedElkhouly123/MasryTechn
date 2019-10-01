e3d86282949d5f5bcf287bff4dedeb59440f3c81
moh <MohamedElkhouly123>
2019-10-02 00:48:54 +0200

    new

---
+++ app/src/main/java/com/nglah/masrytechn/Nagla_Location.java
@@ -0,0 +1,14 @@
+package com.nglah.masrytechn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Nagla_Location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nagla__location);
+    }
+}
