c1e3b1b9f74204c24190b227cbdc74d87642a73e
moh <MohamedElkhouly123>
2019-10-01 02:19:17 +0200

    new

---
+++ app/src/main/java/com/nglah/masrytechn/drive_profile.java
@@ -0,0 +1,14 @@
+package com.nglah.masrytechn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class drive_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drive_profile);
+    }
+}
