55b22878b01098c0de144f2969c0e4dec5f90257
moh <MohamedElkhouly123>
2019-10-03 04:38:36 +0200

    new

---
+++ app/src/main/java/com/nglah/masrytechn/view/choose_place/Choose_Place_Inside_City.java
@@ -0,0 +1,16 @@
+package com.nglah.masrytechn.view.choose_place;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class Choose_Place_Inside_City extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose__place__inside__city);
+    }
+}
---
+++ app/src/main/java/com/nglah/masrytechn/view/choose_place/Choose_Place_Outside_City.java
@@ -0,0 +1,16 @@
+package com.nglah.masrytechn.view.choose_place;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class Choose_Place_Outside_City extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose__place__outside__city);
+    }
+}
