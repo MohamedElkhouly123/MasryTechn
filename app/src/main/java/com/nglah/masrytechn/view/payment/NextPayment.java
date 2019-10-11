3931df200082db56638f2979691ae2b4920c0c25
moh <MohamedElkhouly123>
2019-10-11 02:55:47 +0200

    new

---
+++ app/src/main/java/com/nglah/masrytechn/view/payment/NextPayment.java
@@ -0,0 +1,16 @@
+package com.nglah.masrytechn.view.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nglah.masrytechn.R;
+
+public class NextPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_payment);
+    }
+}
